aad754c350c8d0f8845e93e79302ea38eeb9216e
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-05-14 00:42:48 +0000

    h1

---
+++ src/js/component/ContactCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ContactCard = () => {
+    return (
+<>
+<div>
+    <div className="contact">
+        <img />
+        <div className="Contact-info">
+        <p className="contact-name">Tony Nunez</p>
+        <p className="contact-phone">305-900-0149</p>
+        <p className="contact-address">123 Mia terr</p>
+        <p className="contact-email">tony@email.com</p>
+        </div>    
+        </div>    
+    </div>
+</>
+
+    )
+}
+
+export default ContactCrad;
---
+++ src/js/views/Contacts.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ContactCard from "../component/ContactCard.jsx";
+
+const Contacts = () => {
+    return (
+<>
+<div>
+    <AddContact />
+    
+
+</div>
+</>
+
+    )
+}
+
+export default Contacts;
